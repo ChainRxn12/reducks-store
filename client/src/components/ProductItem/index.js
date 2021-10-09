6eb01fa3ce602880cfd04a39c62b042ad8a89ed8
Billy Dorman <b.d.dorman12@gmail.com>
2021-10-09 10:02:37 -0500

    remove usestorecontext, add usedispatch, useselector to productitem

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
+// remove useStoreContext
+// import { useStoreContext } from "../../utils/GlobalState";
+// import useDispatch, useSelector
+import { useDispatch, useSelector } from 'react-redux';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
+  // remove useStoreContext()
+  // const [state, dispatch] = useStoreContext();
+  // add useDispatch, dispatch variable, useSelector
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
 
   const {
     image,
