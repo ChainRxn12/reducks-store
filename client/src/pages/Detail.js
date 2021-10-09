5a0a97f6f1e1c8d6f9f72fa0236b4fa17e29f218
Billy Dorman <b.d.dorman12@gmail.com>
2021-10-09 10:24:03 -0500

    remove usecontextstore, add usedispatch, useselector to details

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,7 +3,10 @@ import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
+// remove useStoreContext
+// import { useStoreContext } from '../utils/GlobalState';
+// import useDispatch, useSelector
+import { useDispatch, useSelector } from 'react-redux';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -15,7 +18,11 @@ import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  // remove useStoreContext
+  // const [state, dispatch] = useStoreContext();
+  // add dispatch variable, useDispatch() function
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
